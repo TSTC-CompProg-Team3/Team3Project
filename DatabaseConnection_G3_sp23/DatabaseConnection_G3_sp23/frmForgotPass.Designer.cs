486f62577ae265e2e34a200c9884442cf8aaa35b
Cory Smith <coryevansmith@gmail.com>
2023-02-07 16:17:30 -0600

    Working on forgot password

--- DatabaseConnection_G3_sp23/DatabaseConnection_G3_sp23/frmForgotPass.Designer.cs
+++ DatabaseConnection_G3_sp23/DatabaseConnection_G3_sp23/frmForgotPass.Designer.cs
@@ -140,6 +140,7 @@
             this.btnCancel.TabIndex = 16;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // frmForgotPass
             // 
--- DatabaseConnection_G3_sp23/DatabaseConnection_G3_sp23/frmForgotPass.cs
+++ DatabaseConnection_G3_sp23/DatabaseConnection_G3_sp23/frmForgotPass.cs
@@ -29,32 +29,36 @@ namespace DatabaseConnection_G3_sp23
         {
             string email = tbxEmail.Text;
             DatabaseConnection database = new DatabaseConnection();
-            database.OpenDatabase();
 
-            try
+            foreach (clsUser user in database.userList)
             {
-                resetCode = GenerateResetCode();
+                if (tbxEmail.Text == user.email){
+                    try
+                    {
+                        resetCode = GenerateResetCode();
 
-                database.StoreResetCodeInDatabase(email, resetCode);
+                        database.StoreResetCodeInDatabase(email, resetCode);
 
-                MailMessage message = new MailMessage("noreply@example.com", email);
-                message.Subject = "Password Reset Code";
-                message.Body = $"Your password reset code is: {resetCode}";
+                        MailMessage message = new MailMessage("noreply@example.com", email);
+                        message.Subject = "Password Reset Code";
+                        message.Body = $"Your password reset code is: {resetCode}";
 
-                SmtpClient smtp = new SmtpClient("smtp.gmail.com");
-                smtp.Credentials = new NetworkCredential("tstccompprogteam3@gmail.com", "ibhkovptxxlrtndr");
-                smtp.EnableSsl = true;
-                smtp.Port = 587;
-                smtp.Send(message);
+                        SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+                        smtp.Credentials = new NetworkCredential("tstccompprogteam3@gmail.com", "ibhkovptxxlrtndr");
+                        smtp.EnableSsl = true;
+                        smtp.Port = 587;
+                        smtp.Send(message);
 
-                MessageBox.Show("Password reset code sent to email.");
+                        MessageBox.Show("Password reset code sent to email.");
 
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("An error occurred while sending the reset code: " + ex.Message);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("An error occurred while sending the reset code: " + ex.Message);
 
 
+                    }
+                }
             }
         }
 
@@ -71,6 +75,11 @@ namespace DatabaseConnection_G3_sp23
             {
 
             }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 
