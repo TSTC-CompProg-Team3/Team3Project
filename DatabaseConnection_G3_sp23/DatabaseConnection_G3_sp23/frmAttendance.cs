282d505489ecf6046740e224dd9a1705aca196f9
Michael Averitt <maveritt@mymail.tstc.edu>
2023-02-14 21:29:39 -0600

    Basic attendance functions

--- DatabaseConnection_G3_sp23/DatabaseConnection_G3_sp23/frmAttendance.cs
+++ DatabaseConnection_G3_sp23/DatabaseConnection_G3_sp23/frmAttendance.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace DatabaseConnection_G3_sp23
 {
     public partial class frmAttendance : Form
     {
+        //private DataGridView dgvAttendance= new DataGridView();
+        private BindingSource binding1 = new BindingSource();
+
+
         public frmAttendance()
         {
             InitializeComponent();
@@ -19,10 +24,42 @@ namespace DatabaseConnection_G3_sp23
 
         public void SetupDataGrid()
         {
-            dgvAttendance.Name = "Attendance";
-            dgvAttendance.Columns[0].Name = "Student";
-            dgvAttendance.Columns[1].Name = "Date";
-            dgvAttendance.Columns[2].Name = "Attendance";
+            binding1.DataSource = DatabaseQuery("SELECT * FROM team3sp232330.Attendance;");
+            dgvAttendance.DataSource = binding1;
+        }
+
+        public DataTable DatabaseQuery(String SQLcom)
+        {
+            String connectionString = "Data Source=3.130.26.194;Initial Catalog=inew2330gsp23;User ID=team3sp232330;Password=fyMU9QpqmW";
+            SqlConnection dBConnect = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(SQLcom, dBConnect);
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            return table;
+        }
+
+        private void frmAttendance_Load(object sender, EventArgs e)
+        {
+            SetupDataGrid();
+        }
+
+        private void dgvAttendance_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            //DGV Column header names
+            dgvAttendance.Columns[0].HeaderCell.Value = "Student";
+            dgvAttendance.Columns[1].HeaderCell.Value = "Date";
+            dgvAttendance.Columns[2].HeaderCell.Value = "Present";
+
+            //Loop and style each column
+            foreach (DataGridViewColumn col in dgvAttendance.Columns)
+            {
+                col.Width = 210;
+                col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            }
         }
     }
 }
